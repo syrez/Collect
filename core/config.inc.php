6752471098e505ccac65b0299af60dc8f7689e27
syrez <vinkeey@gmail.com>
2015-07-15 15:45:11 +0200

    initial commit

---
+++ core/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*DATABASE
+*/
+define('DB_TYPE','mysql');
+define('DB_HOST','localhost');
+define('DB_NAME','collectify');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+
+/**
+ * Redbean config
+ */
+define('REDBEAN_MODEL_PREFIX','\\Collectify\\Model\\');
